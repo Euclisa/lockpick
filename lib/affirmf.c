81d0ebacf2afeb105308a62188200fd2a44f8cca
Matvey Klochihin <matveiklochihin@gmail.com>
2024-01-13 15:24:14 +0300

    Improved 'affirmf' taking out known arguments from ellipsis.

--- lib/affirmf.c
+++ lib/affirmf.c
@@ -1,21 +1,15 @@
 #include "affirmf.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 
 
-inline void __affirmf(bool cond, const char *format, ...)
+inline void __affirmf(bool cond, const char *format, const char *cond_str, const char *file_str, uint64_t line_num, const char *func_str, ...)
 {
     if(cond)
         return;
 
     va_list args;
-    va_start(args, format);
-
-    const char *cond_str = va_arg(args,const char*);
-    const char *file_str = va_arg(args,const char*);
-    uint64_t line_num = va_arg(args,uint64_t);
-    const char *func_str = va_arg(args,const char*);
+    va_start(args, func_str);
 
     char msg[__LP_MAX_AFFIRMF_MSG_SIZE+1] = {0};
     if(format)
